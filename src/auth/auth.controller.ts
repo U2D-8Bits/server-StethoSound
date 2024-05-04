d7635406dcf8797089f21404b586b5c19e6a59db
U2D8Bits <u2d.8bits@gmail.com>
2024-05-04 15:30:50 -0500

    check-token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,6 +7,8 @@ import { AuthService } from './auth.service';
 
 import { CreateUserDto, LoginDto, UpdateAuthDto, RegisterUserDto } from './dto';
 import { AuthGuard } from './guards/auth.guard';
+import { User } from './entities/user.entity';
+import { LoginResponse } from './interfaces';
 
 @Controller('auth')
 export class AuthController {
@@ -18,19 +20,33 @@ export class AuthController {
     return this.authService.create(createUserDto);
   }
 
-  // Ruta para login
+  // EndPoint para login
   @Post('/login')
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
 
-  //Ruta para registro
+  //EndPoint para registro
   @Post('/register')
   register(@Body() registerDto: RegisterUserDto){
     return this.authService.register(registerDto);
   }
 
-  //Ruta para obtener todos los usuarios
+  //EndPoint para hacer un check del token
+  @UseGuards(AuthGuard)
+  @Get('/check-token')
+  checkToken( @Request() req: Request): LoginResponse{
+    
+    const user = req['user'] as User;
+
+    return {
+      user,
+      token: this.authService.getJwtToken({id: user._id})
+    }
+
+  }
+
+  //EndPoint para obtener todos los usuarios
   @UseGuards(AuthGuard)
   @Get()
   findAll( @Request() req: Request ) {
@@ -39,16 +55,19 @@ export class AuthController {
     return this.authService.findAll();
   }
 
+  //EndPoint para obtener un usuario por id
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.authService.findOne(+id);
   }
 
+  //EndPoint para actualizar un usuario
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
     return this.authService.update(+id, updateAuthDto);
   }
 
+  //EndPoint para eliminar un usuario
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.authService.remove(+id);
