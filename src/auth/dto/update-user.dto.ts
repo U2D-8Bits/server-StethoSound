c1a2e56ee838ff27e06c78e448fef16cda666bf4
U2D8Bits <u2d.8bits@gmail.com>
2024-05-06 20:03:01 -0500

    UpdateUser Server

--- src/auth/dto/update-user.dto.ts
+++ src/auth/dto/update-user.dto.ts
@@ -3,4 +3,29 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    
+    @IsString()
+    name: string;
+    
+    @IsString()
+    lastname: string;
+
+    @IsString()
+    username: string;
+
+    @MaxLength(6)
+    password: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsPhoneNumber('EC')
+    phone: string;
+
+    @Length( 10, 10)
+    ced: string;
+
+    @IsString()
+    roles: string[];
+}
